6a05ea9706274dd1c05aeb545255821e9698bf0e
Titon <bruno.titon@live.com>
2020-05-31 14:00:09 -0300

    2020-05-31 Organization of the result button and insert of the code for calculate

--- js/funciones.js
+++ js/funciones.js
@@ -1,36 +1,63 @@
-// function soma(a, b) {
-//     return a + b;  
-// }
-
-// function resultador() {
-//     let a = parseFloat(document.getElementById("firstValue").value);
-//     let b = parseFloat(document.getElementById("secondValue").value);
-//     document.getElementById("result").innerHTML = soma(a, b);    
-// }
-
 // function openDropdown() {
-//     document.getElementsByClassName("dropdown-content").style.display = "block";
-// }
+    //     document.getElementsByClassName("dropdown-content").style.display = "block";
+    // }
 
-function GetClassName(className) {
-    let nameClass = document.querySelector(className);        
-    return nameClass
-}
+    function GetClassName(className) {
+        let nameClass = document.querySelector(className);        
+        return nameClass
+    }
+    
+    
+    //Button Names
+    function AdictionButton() {    
+        GetClassName('.dropbtn').textContent = "Adiction(+)";
+    }
+    
+    function SubtractionButton() {
+        GetClassName('.dropbtn').textContent = "Subtraction(-)";
+    }
+    
+    function MultiplicationButton() {
+        GetClassName('.dropbtn').textContent = "Multiplication(*)";
+    }
+    
+    function DivisionButton() {
+        GetClassName('.dropbtn').textContent = "Division(/)";
+    }
+    
+    function Addiction(a, b) {
+        return a + b;  
+    }
 
+    function Subtraction(a, b) {
+        return a - b;
+    }
 
-//Button Names
-function AdictionButton() {    
-    GetClassName('.dropbtn').textContent = "Adiction(+)";
-}
+    function Multiplication(a, b) {
+        return a * b;
+    }
 
-function SubtractionButton() {
-    GetClassName('.dropbtn').textContent = "Subtraction(-)";
-}
-
-function MultiplicationButton() {
-    GetClassName('.dropbtn').textContent = "Multiplication(*)";
-}
-
-function DivisionButton() {
-    GetClassName('.dropbtn').textContent = "Division(/)";
-}
+    function Division(a, b) {
+        return a / b;
+    }
+    
+    function ResolveForMe() {
+        let a = parseFloat(document.getElementById("firstValue").value);
+        let b = parseFloat(document.getElementById("secondValue").value);
+        
+        if (GetClassName('.dropbtn').textContent === "Adiction(+)") {
+            document.getElementById("result").innerHTML = Addiction(a, b);  
+            GetClassName('.dropbtn').textContent = 'Operation';  
+        } else if (GetClassName('.dropbtn').textContent === "Subtraction(-)") {
+            document.getElementById("result").innerHTML = Subtraction(a, b);    
+            GetClassName('.dropbtn').textContent = 'Operation';
+        } else if (GetClassName('.dropbtn').textContent === "Multiplication(*)") {
+            document.getElementById("result").innerHTML = Multiplication(a, b);    
+            GetClassName('.dropbtn').textContent = 'Operation';
+        } else if (GetClassName('.dropbtn').textContent === "Division(/)") {
+            document.getElementById("result").innerHTML = Division(a, b);    
+            GetClassName('.dropbtn').textContent = 'Operation';
+        } else {
+            window.alert("The operator selected is invalid !");
+        }
+    }
